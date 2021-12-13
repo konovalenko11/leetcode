a65fdc64b207eb7299d4b576a58b920bdad73f22
Ivan Konovalenko <konovalenko11@gmail.com>
2021-12-12 16:39:48 -0800

    Test

---
+++ problems/descriptions.md
@@ -0,0 +1,15 @@
+# Problem Descriptions
+
+<details>
+<summary>1991. Find the Middle Index in Array [[python](problems/python/1991-find-the-middle-index-in-array/main.py)]</summary>
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where `nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1]`.
+
+If `middleIndex == 0`, the left side sum is considered to be **0**. Similarly, if `middleIndex == nums.length - 1`, the right side sum is considered to be **0**0.
+
+Return the leftmost `middleIndex` that satisfies the condition, or **-1** if there is no such index.
+</details>  
+
+
+
---
+++ problems/python/1991-find-the-middle-index-in-array/main.py
@@ -0,0 +1,50 @@
+''' 1991. Find the Middle Index in Array (Easy)
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if 
+middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is 
+no such index.
+'''
+
+from typing import List
+
+class Solution:
+  def find_middle_index(self, nums: List[int]) -> int:
+    sum_before = 0
+    sum_after = 0
+    num_elements = len(nums)
+    index = 0
+    reverse_index = num_elements - 1
+
+    for i in range(num_elements):
+
+      if sum_before >= sum_after:
+        sum_after += nums[reverse_index]
+        reverse_index -= 1
+      else:
+        sum_before += nums[index]
+        index += 1
+
+      print(f'[{i}]: Forward[{index}]: sum[{sum_before}]; '
+        f'Reverse[{reverse_index}]: sum[{sum_after}]')
+
+      if (index == reverse_index):
+        return index if (sum_before == sum_after) else -1
+
+
+f = Solution()
+
+nums = [2,3,-1,8,4]
+print(f'Input array: {nums}')
+print(f'Answer: [{f.find_middle_index(nums)}]')
+
+nums = [1, 2, 3, 4, -5, 5, 5, 0]
+print(f'Input array: {nums}')
+print(f'Answer: [{f.find_middle_index(nums)}]')
+
+nums = [20, -10, 5, 5]
+print(f'Input array: {nums}')
+print(f'Answer: [{f.find_middle_index(nums)}]')
