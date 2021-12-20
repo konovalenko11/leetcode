32e1c627119c48753eefc6397d15d1773b18515d
Ivan Konovalenko <konovalenko11@gmail.com>
2021-12-19 18:10:49 -0800

    WIP. We're close.

--- problems/python/498-diagonal-traverse/main.py
+++ problems/python/498-diagonal-traverse/main.py
@@ -2,62 +2,85 @@ from typing import List
 
 class Solution:
   def diagonal_traverse(self, mat: List[List[int]]) -> List[int]:
+    # Output list.
+    result_list = []
 
-    # Initial position.
-    x = 0
-    y = 0
-
-    # Delta by X (rows) and Y (columns).
-    xd = -1
-    yd = 1
-    direction = 0
+    if not mat:
+      return result_list
 
     # Rows, columns.
     nrows = len(mat)
     ncols = len(mat[0])
+    nelements = nrows * ncols
 
-    # First value will be 0, 0.
-    result_list = [mat[x][y]] 
+    # if array is flat or empty, then there is not need to traverse the matrix 
+    # by diagonal.
+    if nrows < 2 or ncols < 2:
+      for row in mat:
+        for element in row:
+          result_list.append(element)
+      return result_list
 
-    for _ in range(nrows * ncols - 2):
-        xnew = x + xd
-        ynew = y + yd
+    # Initial position.
+    x = 0
+    y = 1
 
-        if xnew >= 0 and xnew < nrows:
-          x = xnew
-        else:
-          direction -= 1
+    # Initial vector delta by X (rows) and Y (columns).
+    xd = 1
+    yd = -1
 
-        if ynew >= 0 and ynew < ncols:
-          y = ynew
-        else:
-          direction -= 1
+    # To simpify the logic we want to exclude first and last elements from the 
+    # traversing as they are known to be at the beginning and at the end of the 
+    # result list. So the starting (initial) point will be (0, 1).
+    # Including it into the result array as well together with the (0, 0).
+    result_list = [mat[0][0], mat[x][y]] 
 
-        # If we can't move by both x and y, moving cursor by coordinate that had 
-        # negative delta before. In each moment exactly one directive has 
-        # negative delta.
-        if direction == -2:
-          if xd == -1:
-            x += 1
-          else:
-            y += 1
+    # We expect n-3 transitions as first, second, and last points are known.
+    for _ in range(nelements - 3):
+      xnew = x + xd
+      ynew = y + yd
 
-        # If at least for one directive we reached the boundary, we're changing 
-        # direction.
-        if direction < 0:
-          xd *= -1
-          yd *= -1
-          direction = 0
+      # Defining coordinates. If new coordinate is within the matrix, then 
+      # assigning it. Otherwise (if we came to the box edge), we need to change 
+      # the direction and move vertically: x = x + 1, ynew = y.
+      if (xnew >= 0 and xnew < nrows) and (ynew >= 0 and ynew < ncols):
+        x = xnew
+        y = ynew
+      else:
+        x += 1
+        xd *= -1
+        yd *= -1
 
-        result_list.append(mat[x][y])
-        print(f'[{x}][{y}]: {mat[x][y]}')
+      result_list.append(mat[x][y])
 
-    result_list.append(mat[nrows-1][ncols-1])
+    # Qppending last element. Using "-1" index for shorteness.
+    # But it the same as: mat[nrows - 1][ncols - 1].
+    result_list.append(mat[-1][-1])
 
     return result_list
 
 f = Solution()
 
-mat = [[1,2,3],[4,5,6],[7,8,9]]
+# mat = [[1,2,3],[4,5,6],[7,8,9]]
+# print(f'Input array: {mat}')
+# print(f'Answer: {f.diagonal_traverse(mat)}')
+
+mat = [[2,5,8],[4,0,-1]]
 print(f'Input array: {mat}')
-print(f'Answer: [{f.diagonal_traverse(mat)}]')
+print(f'Answer: {f.diagonal_traverse(mat)}')
+
+# mat = []
+# print(f'Input array: {mat}')
+# print(f'Answer: {f.diagonal_traverse(mat)}')
+
+# mat = [[]]
+# print(f'Input array: {mat}')
+# print(f'Answer: {f.diagonal_traverse(mat)}')
+
+# mat = [[1],[2], [3]]
+# print(f'Input array: {mat}')
+# print(f'Answer: {f.diagonal_traverse(mat)}')
+
+# mat = [[1, 2, 3]]
+# print(f'Input array: {mat}')
+# print(f'Answer: {f.diagonal_traverse(mat)}')
