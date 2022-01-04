4523236ee0e9de563d2029295ef3428eff2c589e
Ivan Konovalenko <konovalenko11@gmail.com>
2022-01-03 21:31:10 -0800

    28 - Likely works

--- problems/python/28-implement-strstr/solution.py
+++ problems/python/28-implement-strstr/solution.py
@@ -10,9 +10,11 @@ class Solution:
     h_len = len(haystack)
     n_len = len(needle)
     h_idx = 0
+    n_search_start = 0
 
     while h_idx < h_len:
-      print(f'[{h_idx}]: {haystack[h_idx]}')
+      # DEV
+      # print('-----') 
 
       # If haven't found match till that time, there is no sense to proceed.
       if h_idx + n_len > h_len:
@@ -21,11 +23,25 @@ class Solution:
       # Indicates if loop was interrupted. If not, it means that we found a 
       # match.
       search_broken = False
+      same_chars = True
 
-      for n_idx in range(n_len):
+      for n_idx in range(n_search_start, n_len):
         h_idx_shifted = h_idx + n_idx
+        h_char_shifted = haystack[h_idx_shifted]
 
-        if haystack[h_idx_shifted] != needle[n_idx]:
+        # Checks if we had the same char all the time.
+        if same_chars and n_idx > 0:
+          same_chars = (h_char_shifted == haystack[h_idx_shifted - 1])
+
+        # DEV
+        # print(
+        #   f'[{h_idx}][{haystack[h_idx]}]: '
+        #   f'[{h_idx_shifted}][{h_char_shifted}][{needle[n_idx]}]'
+        #   f'[{same_chars}][{n_search_start}]'
+        # )
+
+        if h_char_shifted != needle[n_idx]:
+          n_search_start = n_idx if same_chars else 0
           search_broken = True
           break
 
@@ -38,44 +54,44 @@ class Solution:
 
 f = Solution()
 
-# haystack = 'a'*100000 + "b"
-# needle = 'a'*10000 + "b"
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
-
-haystack = 'a'*10 + "b"
-needle = 'a'*2 + "b"
+haystack = 'a'*100000 + "b"
+needle = 'a'*10000 + "b"
 print(f'Input: haystack = "{haystack}", needle = "{needle}"')
 print(f'Answer: {f.strStr(haystack, needle)}')
 
-# haystack = "hello"
-# needle = "ll"
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
+haystack = 'a'*100 + "b"
+needle = 'a'*10 + "b"
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
 
-# haystack = "hello"
-# needle = "lo"
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
+haystack = "hello"
+needle = "ll"
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
 
-# haystack = "hello"
-# needle = "l"
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
+haystack = "hello"
+needle = "lo"
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
 
-# haystack = "mississippi"
-# needle = "issip"
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
+haystack = "hello"
+needle = "l"
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
 
-# haystack = "aaaaa"
-# needle = "bba"
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
+haystack = "mississippi"
+needle = "issip"
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
 
-# haystack = "aaaaa"
-# needle = ""
-# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-# print(f'Answer: {f.strStr(haystack, needle)}')
+haystack = "aaaaa"
+needle = "bba"
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
+
+haystack = "aaaaa"
+needle = ""
+print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+print(f'Answer: {f.strStr(haystack, needle)}')
 
 # dis.dis(f.addBinary)
