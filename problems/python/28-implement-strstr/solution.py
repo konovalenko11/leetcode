eb11369ea8e7c5a8ced1c11e3ce5f9d936f97337
Ivan Konovalenko <konovalenko11@gmail.com>
2022-01-03 20:41:55 -0800

    28 - WIP. Refactoring

--- problems/python/28-implement-strstr/solution.py
+++ problems/python/28-implement-strstr/solution.py
@@ -9,54 +9,73 @@ class Solution:
 
     h_len = len(haystack)
     n_len = len(needle)
+    h_idx = 0
 
-    for h_idx in range(h_len):
+    while h_idx < h_len:
+      print(f'[{h_idx}]: {haystack[h_idx]}')
+
+      # If haven't found match till that time, there is no sense to proceed.
+      if h_idx + n_len > h_len:
+        return -1 
+
+      # Indicates if loop was interrupted. If not, it means that we found a 
+      # match.
+      search_broken = False
+
       for n_idx in range(n_len):
         h_idx_shifted = h_idx + n_idx
 
-        # If not enough chars left in haystack to get the match, returning -1.
-        if h_idx_shifted >= h_len:
-          return -1
-
-        # If chars does not match, then there is no sense to proceed.
         if haystack[h_idx_shifted] != needle[n_idx]:
+          search_broken = True
           break
 
-        if n_idx == n_len - 1:
-          return h_idx
+      if not search_broken:
+        return h_idx
+
+      h_idx += 1
 
     return -1
 
 f = Solution()
 
-haystack = "hello"
-needle = "ll"
+# haystack = 'a'*100000 + "b"
+# needle = 'a'*10000 + "b"
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
+
+haystack = 'a'*10 + "b"
+needle = 'a'*2 + "b"
 print(f'Input: haystack = "{haystack}", needle = "{needle}"')
 print(f'Answer: {f.strStr(haystack, needle)}')
 
-haystack = "hello"
-needle = "lo"
-print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-print(f'Answer: {f.strStr(haystack, needle)}')
+# haystack = "hello"
+# needle = "ll"
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
 
-haystack = "hello"
-needle = "l"
-print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-print(f'Answer: {f.strStr(haystack, needle)}')
+# haystack = "hello"
+# needle = "lo"
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
 
-haystack = "mississippi"
-needle = "issip"
-print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-print(f'Answer: {f.strStr(haystack, needle)}')
+# haystack = "hello"
+# needle = "l"
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
 
-haystack = "aaaaa"
-needle = "bba"
-print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-print(f'Answer: {f.strStr(haystack, needle)}')
+# haystack = "mississippi"
+# needle = "issip"
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
 
-haystack = "aaaaa"
-needle = ""
-print(f'Input: haystack = "{haystack}", needle = "{needle}"')
-print(f'Answer: {f.strStr(haystack, needle)}')
+# haystack = "aaaaa"
+# needle = "bba"
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
+
+# haystack = "aaaaa"
+# needle = ""
+# print(f'Input: haystack = "{haystack}", needle = "{needle}"')
+# print(f'Answer: {f.strStr(haystack, needle)}')
 
 # dis.dis(f.addBinary)
