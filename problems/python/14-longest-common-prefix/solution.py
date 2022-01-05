0e9ef3b427f2ef826de5450516bd7ed57227d132
Ivan Konovalenko <konovalenko11@gmail.com>
2022-01-04 23:54:22 -0800

    Updated v1

--- problems/python/14-longest-common-prefix/solution.py
+++ problems/python/14-longest-common-prefix/solution.py
@@ -3,25 +3,24 @@ from typing import List
 
 class Solution:
   def longestCommonPrefix(self, strs: List[str]) -> str:
-    prefix = strs[0]
+    prefix = list(strs[0])
 
     for str in strs[1:]:
-      new_prefix = []
-
       for i in range(len(prefix)):
         if i >= len(str) or prefix[i] != str[i]:
+          del prefix[i:]
           break
+      
+      if not prefix:
+        break
 
-        new_prefix.append(prefix[i])
-
-      prefix = new_prefix.copy()
     return ''.join(prefix)
 
 f = Solution()
 
-strs = ["flower","flow","flight"]
-print(f'Input: strs = {strs}')
-print(f'Answer: {f.longestCommonPrefix(strs)}')
+# strs = ["flower","flow","flight"]
+# print(f'Input: strs = {strs}')
+# print(f'Answer: {f.longestCommonPrefix(strs)}')
 
 strs = ["flower","xxlow","flight"]
 print(f'Input: strs = {strs}')
