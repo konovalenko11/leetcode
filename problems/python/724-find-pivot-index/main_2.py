a6b97cd3734b961ef687f7508bb594b704d177c4
Ivan Konovalenko <konovalenko11@gmail.com>
2022-12-12 22:59:15 -0800

    Small fix

--- problems/python/724-find-pivot-index/main_2.py
+++ problems/python/724-find-pivot-index/main_2.py
@@ -5,29 +5,3 @@ class Solution:
     
 
     return -1
-
-# f = Solution()
-
-# nums = [2,3,-1,8,4]
-# print(f'Input array: {nums}')
-# print(f'Answer: [{f.find_pivot_index(nums)}]')
-
-# nums = [1, 2, 3, 4, -5, 5, 5, 0]
-# print(f'Input array: {nums}')
-# print(f'Answer: [{f.find_pivot_index(nums)}]')
-
-# nums = [20, -10, 5, 5]
-# print(f'Input array: {nums}')
-# print(f'Answer: [{f.find_pivot_index(nums)}]')
-
-# nums = [1,7,3,6,5,6]
-# print(f'Input array: {nums}')
-# print(f'Answer: [{f.find_pivot_index(nums)}]')
-
-# nums = [1,2,3]
-# print(f'Input array: {nums}')
-# print(f'Answer: [{f.find_pivot_index(nums)}]')
-
-# nums = [2,1,-1]
-# print(f'Input array: {nums}')
-# print(f'Answer: [{f.find_pivot_index(nums)}]')
